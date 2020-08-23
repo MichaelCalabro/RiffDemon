664e7c9f5d10e4370c2252ef274c0d51a8a0c974
MichaelCalabro <mmc.calabro@gmail.com>
2020-08-23 14:56:39 -0400

    Updates

---
+++ precache-manifest.a2b0f7746d37b8dbf6d8768e84d4743d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106b0d4605453f2023942df411bbea85",
+    "url": "/RiffDemon/index.html"
+  },
+  {
+    "revision": "a9b7a4a42c419f944249",
+    "url": "/RiffDemon/static/css/main.677c40d9.chunk.css"
+  },
+  {
+    "revision": "5c80981a45ff370f27a8",
+    "url": "/RiffDemon/static/js/2.d4056a28.chunk.js"
+  },
+  {
+    "revision": "e58b2fed1a83725c5b0108382043ede5",
+    "url": "/RiffDemon/static/js/2.d4056a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9b7a4a42c419f944249",
+    "url": "/RiffDemon/static/js/main.db6d9a5a.chunk.js"
+  },
+  {
+    "revision": "ec09686b776907651da0",
+    "url": "/RiffDemon/static/js/runtime-main.5176b6bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RiffDemon/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "be1158d7f6f494bae86c0defe0a285f8",
+    "url": "/RiffDemon/static/media/pirulen rg.be1158d7.ttf"
+  }
+]);
